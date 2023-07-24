95899349e7d179f37fa3a131c528a1bc0cc49014
Jerricke <eejerrickw@gmail.com>
2023-07-24 11:36:41 -0600

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = [] #[0, 1, 1, 2]
+    for n in range(length):
+        try: 
+            prevN = fib[n - 1] #fib[3 - 1] => 1
+        except IndexError:
+            prevN = n
+        try:
+            prevN2 = fib[n - 2]
+            if n == 1: 
+                raise IndexError("-1")
+        except IndexError:
+            prevN2 = n
+        fib.append(prevN + prevN2)
+    print(fib)
+
+print_fibonacci(5)
